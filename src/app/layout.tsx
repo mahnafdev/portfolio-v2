e3417ad91f416e61ca7269d29daac7b34c3fb3fa
mahnafdev <mahnafdev@gmail.com>
2026-01-19 01:52:37 +0600

    chore(config): add site metadata and og data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,53 @@ import type { Metadata } from "next";
 import {} from "next/font/google";
 import "./globals.css";
 
+export const metadata: Metadata = {
+	metadataBase: "https://ahnaf.my",
+	alternates: {
+		canonical: "/",
+	},
+	appLinks: {
+		web: { url: "/" },
+	},
+	applicationName: "Ahnaf Portfolio",
+	title: "It's Muhammad Ahnaf",
+	description:
+		"CTO @ Titonex | Software Developer | TypeScript, MERN, Postgres, Next.js, React Native, Prisma, Go, Docker",
+	creator: "Muhammad Ahnaf",
+	robots: { index: true, follow: true },
+	formatDetection: { url: true, email: true, telephone: true, date: false, address: false },
+	openGraph: {
+		type: "website",
+		siteName: "Muhammad Ahnaf Portfolio",
+		title: "Ahnaf Portfolio",
+		url: "/",
+		images: "/og.png",
+		emails: ["mahnaf.swe@gmail.com", "mahnafdev@gmail.com"],
+		phoneNumbers: "+8801710243940",
+		description:
+			"CTO @ Titonex | Software Developer | TypeScript, MERN, Postgres, Next.js, React Native, Prisma, Go, Docker",
+	},
+	keywords: [
+		"portfolio",
+		"portfolio inspiration",
+		"cool portfolio",
+		"good portfolio",
+		"great portfolio",
+		"developer portfolio",
+		"software developer portfolio",
+		"web developer portfolio",
+		"app developer portfolio",
+		"software engineer portfolio",
+		"programmer portfolio",
+		"developer",
+		"software developer",
+		"web developer",
+		"app developer",
+		"software engineer",
+		"programmer",
+	],
+};
+
 export default function RootLayout({
 	children,
 }: Readonly<{
